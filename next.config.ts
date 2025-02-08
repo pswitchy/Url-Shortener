196e646e86f6bde81825a8c7e674006ae8ec3dff
Parth <ps169969@gmail.com>
2025-02-08 22:57:47 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
